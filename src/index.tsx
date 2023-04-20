5683a3c7df1459ab3e082b17ce006d2e7b07a2f1
Tyler Long <tyler4long@gmail.com>
2023-04-20 15:29:47 -0700

    Dynamically change theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import { EditorView, basicSetup } from 'codemirror';
 import { oneDark } from '@codemirror/theme-one-dark';
+import { Compartment } from '@codemirror/state';
+
+const compartment = new Compartment();
 
 const view = new EditorView({
   doc: 'Hello world!',
-  extensions: [basicSetup, oneDark],
+  extensions: [basicSetup, compartment.of(oneDark)],
   parent: document.body,
 });
+
+setTimeout(
+  () =>
+    view.dispatch({
+      effects: compartment.reconfigure([]),
+    }),
+  1000,
+);
